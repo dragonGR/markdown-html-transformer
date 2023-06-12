1dfb63b3ff4a9c584a27a48eadee561039affd5c
dragonGR <alex@dragongr.dev>
2023-06-12 09:05:10 +0300

    markdown-html-transformer: 1.0.3: Add plugins option & Error handling
    - Use Linkify, it will 'convert' URLs into clickable ones
    - Include Typographer as well.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const MarkdownIt = require('markdown-it');
 const md = new MarkdownIt();
 
-function convertMarkdownToHTML(markdownContent) {
-  return md.render(markdownContent);
+function convertMarkdownToHTML(markdownContent, options = {}) {
+  try {
+    const { linkify = true, typographer = true } = options;
+
+    md.set({ linkify, typographer });
+
+    const html = md.render(markdownContent);
+
+    return html;
+  } catch (error) {
+    console.error('Error converting Markdown to HTML:', error);
+    return 'Error converting Markdown to HTML';
+  }
 }
 
 module.exports = { convertMarkdownToHTML };
